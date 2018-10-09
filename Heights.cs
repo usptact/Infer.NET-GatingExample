828129f128903ab45fea542ad1703e2455cf5388
Vladislavs Dovgalecs <vladislavsd@speak.ai>
2018-10-08 18:27:08 -0700

    Update to open source Infer.NET

--- Heights.cs
+++ Heights.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using MicrosoftResearch.Infer.Models;
-using MicrosoftResearch.Infer;
+using Microsoft.ML.Probabilistic.Models;
 
 namespace Heights
 {
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Gating example for inference using Infer.NET
 
 Machine Learning Summer School 2009 workshop example in Infer.NET framework.
 http://mlg.eng.cam.ac.uk/mlss09/material.htm
+
+Update to open source Infer.NET.
